97f45cacae0c9525b0f51be65ab3d82b3885fcb5
Yohei Goto <yohei_goto@diveintocode.jp>
2022-05-22 11:58:36 +0900

    :peach: prize a sender some eth randomly

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -1,70 +1,80 @@
 // SPDX-License-Identifier: UNLICENSED
+
 pragma solidity ^0.8.4;
+
 import "hardhat/console.sol";
+
 contract WavePortal {
     uint256 totalWaves;
-    /*
-    * NewWaveイベントの作成
-    */
+    uint256 private seed;
+
     event NewWave(address indexed from, uint256 timestamp, string message);
 
-    /*
-    * Waveという構造体を作成。
-    * 構造体の中身は、カスタマイズすることができます。
-    */
     struct Wave {
-        address waver; //「👋（wave）」を送ったユーザーのアドレス
-        string message; // ユーザーが送ったメッセージ
-        uint256 timestamp; // ユーザーが「👋（wave）」を送った瞬間のタイムスタンプ
+        address waver;
+        string message;
+        uint256 timestamp;
     }
 
-    /*
-    * 構造体の配列を格納するための変数wavesを宣言。
-    * これで、ユーザーが送ってきたすべての「👋（wave）」を保持することができます。
-    */
     Wave[] waves;
-    constructor() payable {
-        console.log("WavePortal - Smart Contract!");
-    }
 
     /*
-    * _messageという文字列を要求するようにwave関数を更新。
-    * _messageは、ユーザーがフロントエンドから送信するメッセージです。
-    */
-    function wave(string memory _message) public {
-        totalWaves += 1;
-        console.log("%s waved w/ message %s", msg.sender, _message);
-        /*
-        * 「👋（wave）」とメッセージを配列に格納。
-        */
-        waves.push(Wave(msg.sender, _message, block.timestamp));
-        /*
-        * コントラクト側でemitされたイベントに関する通知をフロントエンドで取得できるようにする。
-        */
-        emit NewWave(msg.sender, block.timestamp, _message);
-
-        /*
-        * 「👋（wave）」を送ってくれたユーザーに0.0001ETHを送る
-        */
-        uint256 prizeAmount = 0.0001 ether;
-        require(
-            prizeAmount <= address(this).balance,
-            "Trying to withdraw more money than the contract has."
-        );
-        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
-        require(success, "Failed to withdraw money from contract.");
-    }
-    /*
-     * 構造体配列のwavesを返してくれるgetAllWavesという関数を追加。
-     * これで、私たちのWEBアプリからwavesを取得することができます。
+     * "address => uint mapping"は、アドレスと数値を関連付ける
      */
+    mapping(address => uint256) public lastWavedAt;
+
+    constructor() payable {
+        console.log("We have been constructed!");
+        /*
+         * 初期シードの設定
+         */
+        seed = (block.timestamp + block.difficulty) % 100;
+    }
+
+    function wave(string memory _message) public {
+        /*
+         * 現在ユーザーがwaveを送信している時刻と、前回waveを送信した時刻が15分以上離れていることを確認。
+         */
+        require(
+            lastWavedAt[msg.sender] + 15 minutes < block.timestamp,
+            "Wait 15m"
+        );
+
+        /*
+         * ユーザーの現在のタイムスタンプを更新する
+         */
+        lastWavedAt[msg.sender] = block.timestamp;
+
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        /*
+         *  ユーザーのために乱数を設定
+         */
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+
+        if (seed <= 50) {
+            console.log("%s won!", msg.sender);
+
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more money than they contract has."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
+
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
     function getAllWaves() public view returns (Wave[] memory) {
         return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
-        // コントラクトが出力する値をコンソールログで表示する。
-        console.log("We have %d total waves!", totalWaves);
         return totalWaves;
     }
 }
